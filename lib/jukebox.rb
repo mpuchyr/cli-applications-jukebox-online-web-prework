4bac84534a81554adb4d08b216954d6b02e1f742
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-21 01:09:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,5 +20,35 @@ def help
 end
 
 def list(songs)
+  songs.each_with_index do  |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_input = gets.strip
   
+  if (user_input.to_i <= songs.length) && (user_input.to_i > 0)
+    puts "Playing #{songs[user_input.to_i] - 1}"
+  elsif songs.include?(user_input)
+    puts "Playing #{user_input}"
+  else
+    puts "Invalid input, please try again."
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run
+  puts "Please entere a command:"
+  user_input = gets.strip
+  while user_input != "exit" do
+    if user_input == "help"
+      help
+    else 
+  end
+  exit_jukebox
 end
